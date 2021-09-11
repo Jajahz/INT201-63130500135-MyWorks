24f5e0c7471e4bd8c80b8f519fbf6bec407437b1
Asamaporn Pladsri <asamaporn.jajah@mail.kmutt.ac.th>
2021-09-12 03:32:34 +0700

    week5

--- Week5.js
+++ Week5.js
@@ -43,3 +43,69 @@ let product2 = {
 }
 // object have array inside*/
 
+class Dice{
+    
+
+    constructor(gameTitle){
+        
+        console.log('constructor is working')
+        this._diceName = gameTitle;
+        this._faceValue = 0;
+
+        if(gameTitle === undefined){
+            this._diceName = 'Unknown';
+        }else{
+            this._diceName = gameTitle;
+        }
+
+
+    }
+
+    roll(){
+        this._faceValue = Math.floor(Math.random()*6)+1;
+        return this._faceValue;
+    }
+
+    get faceValue(){
+        console.log('getter is working')
+        return this._faceValue;
+    }
+
+    set faceValue(faceValue){
+        console.log('getter is working')
+        this._faceValue = faceValue;
+    }
+
+    get diceName(){
+        return this._diceName;
+    }
+
+    set diceName(diceName){
+        this._diceName = diceName;
+    }
+
+    toString(){
+        return `game = ${this._diceName} faceValue = ${this._faceValue}`
+    }
+}
+
+let dice1 = new Dice('hilo');
+
+// console.log(dice1)
+// console.log(dice1.roll())
+// console.log(dice1.faceValue)
+// console.log(dice1["faceValue"])
+// dice1["faceValue"]=5;
+// console.log(dice1.faceValue)
+console.log(dice1.toString())
+
+let dice2 = new Dice();
+
+// console.log(dice2)
+// console.log(dice2.roll)
+
+Dice.prototype.toString = function(){
+    return `game = ${this._diceName} faceValue = ${this._faceValue}`
+}
+
+console.log(dice2.toString())
